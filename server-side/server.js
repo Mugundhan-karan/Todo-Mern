bb48a94ac5ed26f294e380977246731a6a223376
Mugundhan-karan <119132239+Mugundhan-karan@users.noreply.github.com>
2022-11-27 22:55:53 +0530

    Update server.js

--- server-side/server.js
+++ server-side/server.js
@@ -1,17 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors");
-
 const app = express();
 
 app.use(express.json());
-app.use(cors());
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/mern-todo", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect("mongodb://127.0.0.1:27017/mern-todo",)
   .then(() => console.log("connected to Database"))
   .catch(console.error);
 
